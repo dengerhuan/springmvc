d626e340be338e0fa2b4457c8846a0889857b5fe
dengerhuan <argus.deng@nokia.com>
2017-11-23 00:15:16 +0800

    all log4j2

--- src/main/java/cn/huanuo/controller/ViewController.java
+++ src/main/java/cn/huanuo/controller/ViewController.java
@@ -1,13 +1,17 @@
 package cn.huanuo.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
 
+    private static final Logger logger = LogManager.getLogger(ViewController.class.getName());
     @RequestMapping("kpi")
     public String name() {
+        logger.info("kpi");
         System.out.println("kpi");
         return "kpi";
     }
